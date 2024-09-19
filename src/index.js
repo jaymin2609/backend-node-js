50a96ae06522839653c2389f6a12ca88587096c2
jaymin2609 <jaimin2305@gmail.com>
2024-09-19 17:04:08 +0530

    Utils

--- src/index.js
+++ src/index.js
@@ -1,30 +1,58 @@
 // require('dotenv').config({ path: './env' })
 import dotenv from "dotenv"
 import connectDB from "./db/db.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is runnig on port ${process.env.PORT}`);
+        })
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+    })
+    .catch((error) => {
+        console.log("MONGO DB Connection FAILED : ", error);
+
+    })
 
 
-// import express from "express";
 
-// const app = express()
 
-//     ; (async () => {
-//         try {
-//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//             app.on("error", () => {
-//                 console.error("ERROR : ", error);
-//                 throw error
 
-//             })
-//             app.listen(process.env.PORT, () => {
-//                 console.log(`App is running on port ${process.env.PORT}`);
-//             })
-//         } catch (error) {
-//             console.error("ERROR : ", error);
-//         }
-//     })()
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("errror", (error) => {
+        console.log("ERRR: ", error);
+        throw error
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+    })
+
+} catch (error) {
+    console.error("ERROR: ", error)
+    throw err
+}
+})()
+
+*/
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+const asyncHandler = (reqHandler) => {
+    (req, res, next) => {
+        Promise
+            .resolve(reqHandler(req, res, next))
+            .catch((err) => { next(err) })
+    }
+}
+
+
+export { asyncHandler }
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+
+//     } catch (error) {
+//         res.status(error.code || 500).json(
+//             {
+//                 sucess: false,
+//                 message: error.message
+//             }
+//         )
+
+//     }
+// }
+
